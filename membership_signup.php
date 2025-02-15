8ef91aeec48588ad7c1f0a76cb152fb8e8496911
Sanjay <sg493436@gmail.com>
2025-02-15 16:02:44 +0530

    Added company logo.

--- membership_signup.php
+++ membership_signup.php
@@ -1,58 +1,60 @@
 <?php
-	include_once(__DIR__ . '/lib.php');
-	if(MULTI_TENANTS) redirect(SaaS::signupUrl(), true);
+include_once(__DIR__ . '/lib.php');
+if (MULTI_TENANTS) redirect(SaaS::signupUrl(), true);
 
-	$adminConfig = config('adminConfig');
-	$app_name = APP_TITLE;
+include_once(__DIR__ . '/header.php');
 
-	if(!$cg = sqlValue("select count(1) from membership_groups where allowSignup=1")) {
-		$noSignup = true;
-		echo error_message($Translation['sign up disabled']);
+$adminConfig = config('adminConfig');
+$app_name = APP_TITLE;
+
+if (!$cg = sqlValue("select count(1) from membership_groups where allowSignup=1")) {
+	$noSignup = true;
+	echo error_message($Translation['sign up disabled']);
+	exit;
+}
+
+if (Request::val('signUp')) {
+	// receive data
+	$memberID = is_allowed_username(Request::val('newUsername'));
+	$email = isEmail(Request::val('email'));
+	$password = Request::val('password');
+	$confirmPassword = Request::val('confirmPassword');
+	$groupID = intval(Request::val('groupID'));
+	$custom1 = makeSafe(Request::val('custom1'));
+	$custom2 = makeSafe(Request::val('custom2'));
+	$custom3 = makeSafe(Request::val('custom3'));
+	$custom4 = makeSafe(Request::val('custom4'));
+
+	// validate data
+	if (!$memberID) {
+		echo error_message($Translation['username invalid']);
+		exit;
+	}
+	if (strlen($password) < 4 || trim($password) != $password) {
+		echo error_message($Translation['password invalid']);
+		exit;
+	}
+	if ($password != $confirmPassword) {
+		echo error_message($Translation['password no match']);
+		exit;
+	}
+	if (!$email) {
+		echo error_message($Translation['email invalid']);
+		exit;
+	}
+	if (!sqlValue("select count(1) from membership_groups where groupID='$groupID' and allowSignup=1")) {
+		echo error_message($Translation['group invalid']);
 		exit;
 	}
 
-	if(Request::val('signUp')) {
-		// receive data
-		$memberID = is_allowed_username(Request::val('newUsername'));
-		$email = isEmail(Request::val('email'));
-		$password = Request::val('password');
-		$confirmPassword = Request::val('confirmPassword');
-		$groupID = intval(Request::val('groupID'));
-		$custom1 = makeSafe(Request::val('custom1'));
-		$custom2 = makeSafe(Request::val('custom2'));
-		$custom3 = makeSafe(Request::val('custom3'));
-		$custom4 = makeSafe(Request::val('custom4'));
+	// save member data
+	$needsApproval = sqlValue("select needsApproval from membership_groups where groupID='$groupID'");
+	sql("INSERT INTO `membership_users` set memberID='{$memberID}', passMD5='" . password_hash($password, PASSWORD_DEFAULT) . "', email='{$email}', signupDate='" . @date('Y-m-d') . "', groupID='{$groupID}', isBanned='0', isApproved='" . ($needsApproval == 1 ? '0' : '1') . "', custom1='{$custom1}', custom2='{$custom2}', custom3='{$custom3}', custom4='{$custom4}', comments='member signed up through the registration form.'", $eo);
 
-		// validate data
-		if(!$memberID) {
-			echo error_message($Translation['username invalid']);
-			exit;
-		}
-		if(strlen($password) < 4 || trim($password) != $password) {
-			echo error_message($Translation['password invalid']);
-			exit;
-		}
-		if($password != $confirmPassword) {
-			echo error_message($Translation['password no match']);
-			exit;
-		}
-		if(!$email) {
-			echo error_message($Translation['email invalid']);
-			exit;
-		}
-		if(!sqlValue("select count(1) from membership_groups where groupID='$groupID' and allowSignup=1")) {
-			echo error_message($Translation['group invalid']);
-			exit;
-		}
-
-		// save member data
-		$needsApproval = sqlValue("select needsApproval from membership_groups where groupID='$groupID'");
-		sql("INSERT INTO `membership_users` set memberID='{$memberID}', passMD5='" . password_hash($password, PASSWORD_DEFAULT) . "', email='{$email}', signupDate='" . @date('Y-m-d') . "', groupID='{$groupID}', isBanned='0', isApproved='" . ($needsApproval == 1 ? '0' : '1') . "', custom1='{$custom1}', custom2='{$custom2}', custom3='{$custom3}', custom4='{$custom4}', comments='member signed up through the registration form.'", $eo);
-
-		// admin mail notification
-		/* ---- application name as provided in AppGini is used here ---- */
-		$message = nl2br(
-			"A new member has signed up for {$app_name}.\n\n" .
+	// admin mail notification
+	/* ---- application name as provided in AppGini is used here ---- */
+	$message = nl2br(
+		"A new member has signed up for {$app_name}.\n\n" .
 			"Member name: {$memberID}\n" .
 			"Member group: " . sqlValue("select name from membership_groups where groupID='{$groupID}'") . "\n" .
 			"Member email: {$email}\n" .
@@ -62,53 +64,59 @@
 			($adminConfig['custom2'] ? "{$adminConfig['custom2']}: {$custom2}\n" : '') .
 			($adminConfig['custom3'] ? "{$adminConfig['custom3']}: {$custom3}\n" : '') .
 			($adminConfig['custom4'] ? "{$adminConfig['custom4']}: {$custom4}\n" : '')
-		);
+	);
 
-		if($adminConfig['notifyAdminNewMembers'] == 2 && !$needsApproval)
-			sendmail([
-				'to' => $adminConfig['senderEmail'],
-				'subject' => "[{$app_name}] New member signup",
-				'message' => $message,
-			]);
-		elseif($adminConfig['notifyAdminNewMembers'] >= 1 && $needsApproval)
-			sendmail([
-				'to' => $adminConfig['senderEmail'],
-				'subject' => "[{$app_name}] New member awaiting approval",
-				'message' => $message,
-			]);
+	if ($adminConfig['notifyAdminNewMembers'] == 2 && !$needsApproval)
+		sendmail([
+			'to' => $adminConfig['senderEmail'],
+			'subject' => "[{$app_name}] New member signup",
+			'message' => $message,
+		]);
+	elseif ($adminConfig['notifyAdminNewMembers'] >= 1 && $needsApproval)
+		sendmail([
+			'to' => $adminConfig['senderEmail'],
+			'subject' => "[{$app_name}] New member awaiting approval",
+			'message' => $message,
+		]);
 
-		// hook: member_activity
-		if(function_exists('member_activity')) {
-			$args = [];
-			member_activity(getMemberInfo($memberID), ($needsApproval ? 'pending' : 'automatic'), $args);
-		}
-
-		// redirect to thanks page
-		$redirect = ($needsApproval ? '' : '?redir=1');
-		redirect("membership_thankyou.php$redirect");
-
-		exit;
+	// hook: member_activity
+	if (function_exists('member_activity')) {
+		$args = [];
+		member_activity(getMemberInfo($memberID), ($needsApproval ? 'pending' : 'automatic'), $args);
 	}
 
-	// drop-down of groups allowing self-signup
-	$groupsDropDown = preg_replace('/<option.*?value="".*?><\/option>/i', '', htmlSQLSelect('groupID', "select groupID, concat(name, if(needsApproval=1, ' *', ' ')) from membership_groups where allowSignup=1 order by name", ($cg == 1 ? sqlValue("select groupID from membership_groups where allowSignup=1 order by name limit 1") : 0 )));
-	$groupsDropDown = str_replace('<select ', '<select class="form-control" ', $groupsDropDown);
+	// redirect to thanks page
+	$redirect = ($needsApproval ? '' : '?redir=1');
+	redirect("membership_thankyou.php$redirect");
 
-	include_once(__DIR__ . '/header.php');
+	exit;
+}
+
+// drop-down of groups allowing self-signup
+$groupsDropDown = preg_replace('/<option.*?value="".*?><\/option>/i', '', htmlSQLSelect('groupID', "select groupID, concat(name, if(needsApproval=1, ' *', ' ')) from membership_groups where allowSignup=1 order by name", ($cg == 1 ? sqlValue("select groupID from membership_groups where allowSignup=1 order by name limit 1") : 0)));
+$groupsDropDown = str_replace('<select ', '<select class="form-control" ', $groupsDropDown);
 ?>
 
-<?php if(!$noSignup) { ?>
-	<div class="row">
-		<div class="hidden-xs col-sm-4 col-md-6 col-lg-8" id="signup_splash">
-			<!-- customized splash content here -->
-		</div>
+<?php if (!$noSignup) { ?>
+	<div class="row" style="display: flex; justify-content:center;">
+
+		<!-- <div class="hidden-xs col-sm-4 col-md-6 col-lg-8" id="signup_splash">
+			customized splash content here
+		</div> -->
 
 		<div class="col-sm-8 col-md-6 col-lg-4">
-			<div class="panel panel-success">
+			<div class="panel panel-success" style="border-radius:10px; box-shadow: 0px 8px 15px 5px #848181;">
 
-				<div class="panel-heading">
+				<div class="panel-heading" style="background-color: #c2b28a; color:black;">
 					<h1 class="panel-title"><strong><?php echo $Translation['sign up here']; ?></strong></h1>
 				</div>
+
+
+				<div>
+				<div class="app-logos">
+					<a href="https://iittnif.com"><div class="iittnif-logo"><img src="logos\IITTNiF_Logo.png" alt="" style="width: 100%;"></div></a>
+				</div>
+			</div>
 
 				<div class="panel-body">
 					<form method="post" action="membership_signup.php">
@@ -147,25 +155,35 @@
 						</div>
 
 						<?php
-							if(!$adminConfig['hide_custom_user_fields_during_signup']) {
-								for($cf = 1; $cf <= 4; $cf++) {
-									if($adminConfig['custom'.$cf] != '') {
-										?>
-										<div class="row form-group">
-										   <div class="col-sm-3"><label class="control-label" for="custom<?php echo $cf; ?>"><?php echo htmlspecialchars($adminConfig['custom'.$cf]); ?></label></div>
-										   <div class="col-sm-9"><input class="form-control" type="text" placeholder="<?php echo html_attr($adminConfig['custom'.$cf]); ?>" id="custom<?php echo $cf; ?>" name="custom<?php echo $cf; ?>"></div>
-										</div>
-										<?php
-									}
+						if (!$adminConfig['hide_custom_user_fields_during_signup']) {
+							for ($cf = 1; $cf <= 4; $cf++) {
+								if ($adminConfig['custom' . $cf] != '') {
+						?>
+									<div class="row form-group">
+										<div class="col-sm-3"><label class="control-label" for="custom<?php echo $cf; ?>"><?php echo htmlspecialchars($adminConfig['custom' . $cf]); ?></label></div>
+										<div class="col-sm-9"><input class="form-control" type="text" placeholder="<?php echo html_attr($adminConfig['custom' . $cf]); ?>" id="custom<?php echo $cf; ?>" name="custom<?php echo $cf; ?>"></div>
+									</div>
+						<?php
 								}
 							}
+						}
 						?>
 
-						<div class="row">
-							<div class="col-sm-offset-3 col-sm-6">
-								<button class="btn btn-primary btn-lg btn-block" value="signUp" id="submit" type="submit" name="signUp"><?php echo $Translation['sign up']; ?></button>
+						<div style="display: flex; justify-content:space-around;">
+							<div class="row" style="margin: 10px;">
+								<div class="col-sm-offset-3 col-sm-6">
+									<button name="signIn" type="submit" id="submit" value="signIn" style="border: 1px solid #337ab7; border-radius:8px; padding:6px 15px; font-size:15px; background-color: #951b2a; color:white; min-width:100px;"><?php echo $Translation['sign up']; ?></button>
+								</div>
+							</div>
+
+							<div style="margin:10px;">
+								<a href="https://app.iittnif.in" style="text-decoration: none;">
+									<p style="border: 1px solid #0d6efd; border-radius:8px; padding:6px 15px; font-size:15px; background-color: #0d6efd; color:white; min-width:170px;">Back to Home Page</p>
+								</a>
 							</div>
 						</div>
+
+
 
 					</form>
 				</div> <!-- /div class="panel-body" -->
@@ -177,17 +195,19 @@
 		$j(function() {
 			$j('#username').focus();
 
-			$j('#usernameAvailable, #usernameNotAvailable').click(function() { $j('#username').focus(); });
+			$j('#usernameAvailable, #usernameNotAvailable').click(function() {
+				$j('#username').focus();
+			});
 			$j('#username').on('keyup blur', checkUser);
 
 			/* password strength feedback */
 			$j('#password').on('keyup blur', function() {
 				var ps = passwordStrength($j('#password').val(), $j('#username').val());
 
-				if(ps == 'strong') {
+				if (ps == 'strong') {
 					$j('#password').parents('.form-group').removeClass('has-error has-warning').addClass('has-success');
 					$j('#password').attr('title', <?php echo json_encode($Translation['Password strength: strong']); ?>);
-				} else if(ps == 'good') {
+				} else if (ps == 'good') {
 					$j('#password').parents('.form-group').removeClass('has-success has-error').addClass('has-warning');
 					$j('#password').attr('title', <?php echo json_encode($Translation['Password strength: good']); ?>);
 				} else {
@@ -198,7 +218,7 @@
 
 			/* inline feedback of confirm password */
 			$j('#confirmPassword').on('keyup blur', function() {
-				if($j('#confirmPassword').val() != $j('#password').val() || !$j('#confirmPassword').val().length) {
+				if ($j('#confirmPassword').val() != $j('#password').val() || !$j('#confirmPassword').val().length) {
 					$j('#confirmPassword').parents('.form-group').removeClass('has-success').addClass('has-error');
 				} else {
 					$j('#confirmPassword').parents('.form-group').removeClass('has-error').addClass('has-success');
@@ -207,7 +227,7 @@
 
 			/* inline feedback of email */
 			$j('#email').on('change', function() {
-				if(validateEmail($j('#email').val())) {
+				if (validateEmail($j('#email').val())) {
 					$j('#email').parents('.form-group').removeClass('has-error').addClass('has-success');
 				} else {
 					$j('#email').parents('.form-group').removeClass('has-success').addClass('has-error');
@@ -215,27 +235,31 @@
 			});
 
 			/* validate form before submitting */
-			$j('#submit').click(function(e) { if(!jsValidateSignup()) e.preventDefault(); })
+			$j('#submit').click(function(e) {
+				if (!jsValidateSignup()) e.preventDefault();
+			})
 		});
 
 		var uaro; // user availability request object
 		function checkUser() {
 			// abort previous request, if any
-			if(uaro != undefined) uaro.abort();
+			if (uaro != undefined) uaro.abort();
 
 			reset_username_status();
 
 			uaro = $j.ajax({
-					url: 'checkMemberID.php',
-					type: 'GET',
-					data: { 'memberID': $j('#username').val() },
-					success: function(resp) {
-						if(resp.indexOf('username-available') > -1) {
-							reset_username_status('success');
-						} else {
-							reset_username_status('error');
-						}
+				url: 'checkMemberID.php',
+				type: 'GET',
+				data: {
+					'memberID': $j('#username').val()
+				},
+				success: function(resp) {
+					if (resp.indexOf('username-available') > -1) {
+						reset_username_status('success');
+					} else {
+						reset_username_status('error');
 					}
+				}
 			});
 		}
 
@@ -245,14 +269,14 @@
 				.parents('.form-group')
 				.removeClass('has-error has-success');
 
-			if(status == undefined) return;
-			if(status == 'success') {
+			if (status == undefined) return;
+			if (status == 'success') {
 				$j('#usernameAvailable')
 					.removeClass('hidden')
 					.parents('.form-group')
 					.addClass('has-success');
 			}
-			if(status == 'error') {
+			if (status == 'error') {
 				$j('#usernameNotAvailable')
 					.removeClass('hidden')
 					.parents('.form-group')
@@ -267,19 +291,37 @@
 			var email = $j('#email').val();
 
 			/* user exists? */
-			if(!$j('#username').parents('.form-group').hasClass('has-success')) {
-				modal_window({ message: '<div class="alert alert-danger"><?php echo html_attr($Translation['username invalid']); ?></div>', title: "<?php echo html_attr($Translation['error:']); ?>", close: function() { $j('#username').focus(); } });
+			if (!$j('#username').parents('.form-group').hasClass('has-success')) {
+				modal_window({
+					message: '<div class="alert alert-danger"><?php echo html_attr($Translation['username invalid']); ?></div>',
+					title: "<?php echo html_attr($Translation['error:']); ?>",
+					close: function() {
+						$j('#username').focus();
+					}
+				});
 				return false;
 			}
 
 			/* passwords not matching? */
-			if(p1 != p2) {
-				modal_window({ message: '<div class="alert alert-danger"><?php echo html_attr($Translation['password no match']); ?></div>', title: "<?php echo html_attr($Translation['error:']); ?>", close: function() { $j('#confirmPassword').focus(); } });
+			if (p1 != p2) {
+				modal_window({
+					message: '<div class="alert alert-danger"><?php echo html_attr($Translation['password no match']); ?></div>',
+					title: "<?php echo html_attr($Translation['error:']); ?>",
+					close: function() {
+						$j('#confirmPassword').focus();
+					}
+				});
 				return false;
 			}
 
-			if(!validateEmail(email)) {
-				modal_window({ message: '<div class="alert alert-danger"><?php echo html_attr($Translation['email invalid']); ?></div>', title: "<?php echo html_attr($Translation['error:']); ?>", close: function() { $j('#email').focus(); } });
+			if (!validateEmail(email)) {
+				modal_window({
+					message: '<div class="alert alert-danger"><?php echo html_attr($Translation['email invalid']); ?></div>',
+					title: "<?php echo html_attr($Translation['error:']); ?>",
+					close: function() {
+						$j('#email').focus();
+					}
+				});
 				return false;
 			}
 
@@ -288,7 +330,10 @@
 	</script>
 
 	<style>
-		#usernameAvailable,#usernameNotAvailable{ cursor: pointer; }
+		#usernameAvailable,
+		#usernameNotAvailable {
+			cursor: pointer;
+		}
 	</style>
 
 <?php } ?>

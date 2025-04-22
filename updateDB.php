2e3875b15e4b6314d4a43fefe9af2749dc5adb73
Sanjay <sg493436@gmail.com>
2025-04-22 12:42:21 +0530

    Testing appraisal app.

--- updateDB.php
+++ updateDB.php
@@ -127,11 +127,7 @@
 
 		setupTable('attendence_details_table', []);
 
-		setupTable('leave_table', [
-				"ALTER TABLE leave_table ADD `field1` VARCHAR(40)",
-				"ALTER TABLE `leave_table` CHANGE `field1` `username` VARCHAR(255) NULL ",
-				" ALTER TABLE `leave_table` CHANGE `username` `username` VARCHAR(255) NULL ",
-			]);
+		setupTable('leave_table', []);
 
 		setupTable('work_from_home_table', []);
 		setupIndexes('work_from_home_table', ['approved_by',]);

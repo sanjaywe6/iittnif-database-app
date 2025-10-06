f8d85b62f65e9cd43ef5434de72507d1e80b3f10
Sanjay <sg493436@gmail.com>
2025-10-06 12:54:09 +0530

    Updated Honorarium App.

--- admin/pageServerStatus.php
+++ admin/pageServerStatus.php
@@ -1,6 +1,6 @@
 <?php
 	$appgini_version = '25.12.2050';
-	$generated_ts = '06-10-2025 12:37:54';
+	$generated_ts = '06-10-2025 12:52:16';
 
 	require(__DIR__ . '/incCommon.php');
 
--- updateDB.php
+++ updateDB.php
@@ -176,10 +176,7 @@
 		setupTable('approval_billing_table', []);
 		setupIndexes('approval_billing_table', ['approval_lookup','paid_by',]);
 
-		setupTable('honorarium_claim_table', [
-				"ALTER TABLE `honorarium_claim_table` ADD `created_by_username` VARCHAR(255) NULL ",
-				"ALTER TABLE `honorarium_claim_table` ADD `last_updated_by_username` VARCHAR(255) NULL ",
-			]);
+		setupTable('honorarium_claim_table', []);
 		setupIndexes('honorarium_claim_table', ['coordinated_by_tih_user',]);
 
 		setupTable('all_bank_account_statement_table', []);
